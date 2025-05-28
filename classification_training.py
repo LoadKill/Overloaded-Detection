de11f49d556be61f7cf47a8e8fafcc08ce9b52f6
Ryuchanghoon <fbckdgns3@naver.com>
2025-05-29 01:52:58 +0900

    [#15] fix: Dropout, 데이터 증강 등 과적합 완화 기법 적용

--- classification_training.py
+++ classification_training.py
@@ -12,9 +12,33 @@ import timm
 
 # 미세조정 파라미터
 BATCH_SIZE = 32
-EPOCHS = 300
+EPOCHS = 100
 LR = 0.0001
 IMG_SIZE = 224
+WEIGHT_DECAY = 0.0001  # L2 Regularization 개념
+
+
+# 노이즈 추가 함수(증강 때 사용)
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=0.05):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(mean={self.mean}, std={self.std})"
+
+# Label Smoothing 적용한 BCE Loss
+class SmoothBCEWithLogitsLoss(nn.Module):
+    def __init__(self, smoothing=0.1):
+        super().__init__()
+        self.smoothing = smoothing
+        self.loss_fn = nn.BCEWithLogitsLoss()
+    def forward(self, inputs, targets):
+        targets = targets * (1 - self.smoothing) + 0.5 * self.smoothing
+        return self.loss_fn(inputs, targets)
 
 
 # 학습 데이터셋 증강 파트
@@ -22,7 +46,14 @@ transform_train = transforms.Compose([
     transforms.Resize((IMG_SIZE, IMG_SIZE)),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(30),
+    # 밤 환경 고려 증강 기법
+    transforms.ColorJitter(  
+        brightness = (0.3, 1.0),
+        contrast = (0.5, 1.5),
+        saturation = (0.3, 1.2)
+    ),
     transforms.ToTensor(),
+    AddGaussianNoise(0., 0.02),  # 노이즈 추가 
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
 
@@ -43,13 +74,15 @@ val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
 
 # 사전 학습 모델
 model = timm.create_model('xception', pretrained=True, num_classes=1)
-model.fc = nn.Sequential(model.fc, nn.Sigmoid())
+model.fc = nn.Sequential(nn.Dropout(p = 0.8),  # Dropout 적용
+                         model.fc)  # Sigmoid 제거
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-criterion = nn.BCELoss()
-optimizer = optim.AdamW(model.parameters(), lr=LR)
+criterion = SmoothBCEWithLogitsLoss(smoothing = 0.15)  # 0.1 적용 시 과적합 그래도 심함. 0.15로 적용.
+optimizer = optim.AdamW(model.parameters(), lr = LR, weight_decay = WEIGHT_DECAY)  # Weight Decay 적용.
+
 
 print(f"Using device: {device}")
 
@@ -107,7 +140,7 @@ for epoch in range(EPOCHS):
     # 최고 성능 모델 저장
     if val_acc > best_val_acc:
         best_val_acc = val_acc
-        torch.save(model.state_dict(), 'best_xception_model.pth')
+        torch.save(model.state_dict(), 'xception_model_relieve_overfitting.pth')
 
 # 기록 저장 및 시각화
 results = pd.DataFrame({
@@ -116,7 +149,13 @@ results = pd.DataFrame({
     'val_accuracy': val_accuracies
 }).add_prefix('xception_')
 
-results.to_csv("history_xception.csv", index=False)
+results.to_csv("xception_model_relieve_overfitting.csv", index=False)
+
+best_epoch = results['xception_val_accuracy'].idxmax() + 1
+best_acc = results['xception_val_accuracy'].max()
+
+print('best epoch: ', best_epoch)
+print('best accuracy: ', best_acc)
 
 plt.figure(figsize=(8, 5))
 plt.plot(results.index + 1, results['xception_train_loss'], label='Train Loss')
